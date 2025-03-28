1d1e476c2e0f81015ca141e5e079c88cbc2a8a53
Aravinthan Ganesan <aravinganesan937@gmail.com>
2025-03-28 11:30:26 +0800

    My first website

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import requests
+from datetime import datetime
 
-'''
-# TaxiFareModel front
-'''
+st.title("TaxiFareModel front")
 
 st.markdown('''
 Remember that there are several ways to output content into your web page...
@@ -10,39 +10,35 @@ Remember that there are several ways to output content into your web page...
 Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
 ''')
 
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+st.header("Select Ride Parameters")
 
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
+# 1. Input parameters
+pickup_date = st.date_input("Pickup Date", value=datetime.today())
+pickup_time = st.time_input("Pickup Time", value=datetime.now().time())
+pickup_datetime = f"{pickup_date} {pickup_time}"
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+pickup_longitude = st.number_input("Pickup Longitude", value=-73.985428)
+pickup_latitude = st.number_input("Pickup Latitude", value=40.748817)
+dropoff_longitude = st.number_input("Dropoff Longitude", value=-73.985428)
+dropoff_latitude = st.number_input("Dropoff Latitude", value=40.748817)
+passenger_count = st.number_input("Passenger Count", min_value=1, max_value=10, value=1)
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+# 2. API parameters dictionary
+params = {
+    "pickup_datetime": pickup_datetime,
+    "pickup_longitude": pickup_longitude,
+    "pickup_latitude": pickup_latitude,
+    "dropoff_longitude": dropoff_longitude,
+    "dropoff_latitude": dropoff_latitude,
+    "passenger_count": passenger_count
+}
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
+# 3. API call
 url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+if st.button("Get Fare Prediction"):
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        prediction = response.json()["fare"]
+        st.success(f"Predicted Fare: ${prediction:.2f}")
+    else:
+        st.error("Failed to retrieve prediction from API")
